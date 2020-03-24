78cfe2bd3231317c5ebf2e240d518851975ac1a6
pierwszy program <asterinx35@gmail.com>
2020-03-24 21:07:51 +0100

    pierwszy program hello i tablica args

---
+++ program0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace program0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("witaj, " + name);
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("witaj, " + item);
+            //}
+        }
+    }
+}
